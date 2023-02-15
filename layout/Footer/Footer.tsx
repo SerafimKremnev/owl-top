10c354edad079bd249e61e31cfc1028d9a48970b
Serafim <serafimkremnev1008@gmail.com>
2023-02-15 10:40:57 +0300

    layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {FooterProps} from "./Footer.props";
 import styles from './Footer.module.css'
+import cn from "classnames";
+import { format } from 'date-fns'
 
 const Footer = ({...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            footer
-        </div>
+        <footer {...props} className={cn(styles.footer, props.className)}>
+            <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+            <a href={'#'}>Пользовательское соглашение</a>
+            <a href={'#'}>Политика конфидециальности</a>
+        </footer>
     )
 };
 
