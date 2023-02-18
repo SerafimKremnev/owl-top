9ee81fde2dbe704ce85a1e080ef7a48186e8c9c6
Serafim <serafimkremnev1008@gmail.com>
2023-02-18 13:24:11 +0300

    14

---
+++ page-components/TupPageComponent/TopPageComponent.tsx
@@ -0,0 +1,11 @@
+import {TopPageComponentProps} from "./TopPageComponent.props";
+
+const TopPageComponent = ({page, products, firstCategory}: TopPageComponentProps) => {
+    return (
+        <div>
+            {products && products.length}
+        </div>
+    );
+};
+
+export default TopPageComponent;
